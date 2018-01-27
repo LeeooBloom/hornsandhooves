1fc8d90f12a4aee70aba8184432b4a48613bd1ee
leobloom <adicalzvch@gmail.com>
2018-01-27 22:22:23 +0300

    properties, db sctructure, data sets

---
+++ src/main/java/hornsandhooves/hornsandhooves/HornsandhoovesApplication.java
@@ -0,0 +1,12 @@
+package hornsandhooves.hornsandhooves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HornsandhoovesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HornsandhoovesApplication.class, args);
+	}
+}
